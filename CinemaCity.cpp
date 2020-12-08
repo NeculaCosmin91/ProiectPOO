573da9d8fdbae2d7b4351c873dace491b528ba82
NeculaCosmin91 <neculaionutcosmin@gmail.com>
2020-12-08 22:48:13 +0200

    Update CinemaCity.cpp

--- CinemaCity.cpp
+++ CinemaCity.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<string>
+#include<fstream>
 
+using namespace std;
+class Film
+{
+private:
+	const int id;
+	char* producator;
+	int an;
+	static string nume;
+};
+
+class Bilet
+{
+	const int nrBilete;
+	int pret;
+
+	
+
+};
+
+class Sala
+{
+
+};
+ 
