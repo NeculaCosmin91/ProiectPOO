93e42600acdb647193419bec463e18ad4287e99f
NeculaCosmin91 <neculaionutcosmin@gmail.com>
2020-12-09 22:57:11 +0200

    Update CinemaCity.cpp

--- CinemaCity.cpp
+++ CinemaCity.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 
 using namespace std;
+
 class Film
 {
 private:
@@ -10,6 +11,38 @@ private:
 	char* producator;
 	int an;
 	static string nume;
+
+	Film(int id, char* producator, int an, string nume) :id(1)
+	{
+		if (producator != nullptr)
+		{
+			this->producator = new char[strlen(producator) + 1];
+			strcpy_s(this->producator, strlen(producator) + 1, producator);
+		}
+		else
+		{
+			this -> producator = nullptr;
+		}
+		this->an = an;
+		this->nume = nume;
+	}
+
+	Film(const Film& f) :id(f.id)
+	{
+		if (producator != nullptr)
+		{
+			this->producator = new char[strlen(f.producator) + 1];
+			strcpy_s(this->producator, strlen(f.producator) + 1, f.producator);
+		}
+		else
+		{
+			this->producator = nullptr;
+		}
+		an = f.an;
+		nume = f.nume;
+
+	}
+
 };
 
 class Bilet
@@ -21,6 +54,22 @@ class Bilet
 class Sala
 {
 	const int nrLocuri;
+	char* numeSala;
 	int randuri;
 	
+};
+
+class RezervareOnline
+{
+
+};
+
+class SalaVIP
+{
+
+};
+
+int main()
+{
+
 };
